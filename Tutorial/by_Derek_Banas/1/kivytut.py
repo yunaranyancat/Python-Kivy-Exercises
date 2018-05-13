8ee51c409fe517608542b115ca0dced693331a99
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-05-13 22:32:18 +0800

    hello world in kivy

--- Tutorial/by_Derek_Banas/1/kivytut.py
+++ Tutorial/by_Derek_Banas/1/kivytut.py
@@ -1,9 +1,11 @@
-import kivy
-kivy.require('1.10.0')
+import kivy #import module
+kivy.require('1.10.0') #based on your Kivy version
 
+#import modules from kivy
 from kivy.app import App
 from kivy.uix.button import Label
 
+#inherit kivy apps class
 class HelloKivy(App):
 
     def build(self):
