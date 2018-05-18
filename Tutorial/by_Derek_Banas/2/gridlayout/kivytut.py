b46ae34e0fd2b59e6e968757063ac91e2ec2250b
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-05-18 22:36:02 +0800

    gridlayout

---
+++ Tutorial/by_Derek_Banas/2/gridlayout/kivytut.py
@@ -0,0 +1,14 @@
+import kivy
+kivy.require('1.10.0')
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+class GridLayoutApp(App):
+
+    def build(self):
+        return GridLayout()
+
+glApp = GridLayoutApp()
+
+glApp.run()
