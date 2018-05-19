c674d2f0cb77e89a17425714adaac10424d204df
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-05-19 22:36:15 +0800

    boxlayout

---
+++ Tutorial/by_Derek_Banas/2/boxlayout/kivytut.py
@@ -0,0 +1,14 @@
+import kivy
+kivy.require('1.10.0')
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class BoxLayoutApp(App):
+
+    def build(self):
+        return BoxLayout()
+
+blApp = BoxLayoutApp()
+
+blApp.run()
