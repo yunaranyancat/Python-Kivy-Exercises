bd1a8b70ab52b6e87869bba19aa01bd37ceda470
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-05-24 23:57:49 +0800

    add code to functions

--- Tutorial/by_Derek_Banas/4_ListView_ListAdapter/studentdb.py
+++ Tutorial/by_Derek_Banas/4_ListView_ListAdapter/studentdb.py
@@ -12,14 +12,36 @@ class StudentDB(BoxLayout):
     student_list = ObjectProperty()
 
     def submit_student(self):
-        pass
+        # Get the students name from textInputs
+        student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+        # Add to ListView
+        self.student_list.adapter.data.extend([student_name])
+        # Reset the ListView
+        self.student_list._trigger_reset_populate()
 
     def delete_student(self):
-        pass
+        # If a list item is selected
+        if self.student_list.adapter.selection:
+            # Get the text from the item selected
+            selection = self.student_list.adapter.selection[0].text
+            # Remove the matching item
+            self.student_list.adapter.data.remove(selection)
+            # Reset the ListView
+            self.student_list._trigger_reset_populate()
 
     def replace_student(self):
-        pass
-
+        #If a list item is selected
+        if self.student_list.adapter.selection:
+            # Get the text from the item selected
+            selection = self.student_list.adapter.selection[0].text
+            # Remove the matching item
+            self.student_list.adapter.data.remove(selection)
+            # Get the students name from textInputs
+            student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+            # Add the updated data to the list
+            self.student_list.adapter.data.extend([student_name])
+            # Reset the ListView
+            self.student_list._trigger_reset_populate()
 
 class StudentDBApp(App):
     def build(self):
